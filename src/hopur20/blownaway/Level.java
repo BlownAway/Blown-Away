6988a9b88eaf8edbbee48e248d65c79a9d7cad2b
Thorey <thoreym@gmail.com>
2011-03-20 17:05:22 +0000

    Initial commit

--- src/hopur20/blownaway/Level.java
+++ src/hopur20/blownaway/Level.java
@@ -20,18 +20,33 @@ public class Level {
 	 */
 	
 	private Bomb bomb;
-	private int time;
+	private int time,numColors,numLocations;
 	private int[] locSummary;
 	private int[] colorSummary;
 	
 	/*
-	 * Notkun: level = new Level(i,j,t)
-	 * Fyrir: i er fjöldi mögulegra lita í þessu borði.
-	 * 		  j er fjöldi mögulegra hliða í þessu borði.
-	 * 		  t er tíminn sem gefst til að aftengja sprengju þessa borðs í sekúndum.
+	 * Notkun: level = new Level(n)
+	 * Fyrir: @param n <= 15 er númer borðs.
 	 */
-	public Level(int numColors, int numLocations,int time){
-		this.time = time;
+	public Level(int levelNumber){
+		int difficulty = levelNumber%5;
+		if(levelNumber < 5){
+			numColors = 3;
+			numLocations = 2;
+			this.time = 30-difficulty*5;
+		}
+		else{
+			if(levelNumber <10){
+				numColors = 3;
+				numLocations =4;
+				this.time = 30-difficulty*2;
+			}
+			else{
+				numColors = 4;
+				numLocations = 4;
+				this.time = 45-difficulty*10;
+			}
+		}
 		colorSummary = new int[numColors];
 		locSummary = new int[numLocations];
 		Random generator = new Random();
@@ -39,7 +54,19 @@ public class Level {
 		// for each wire, flip a coin to see if it should be cut.
 		for(int i = 0; i!=locSummary.length;i++){
 			for(int j = 0; j!=colorSummary.length;j++){
-				if(generator.nextDouble()>0.5){
+				double sideSaturation = locSummary[i]/((double) numColors);
+				double colorSaturation = colorSummary[j]/((double) numLocations);
+				
+				double p = 1;
+				if(sideSaturation>0&difficulty<2){
+					p= Math.pow(2*(sideSaturation-0.5), (5-difficulty)*2.0)/2
+					  +Math.pow(2*(colorSaturation-0.5),(5-difficulty)*2.0)/2;
+				}
+				else{
+					p = 1-Math.pow(2*(sideSaturation-0.5), (5-difficulty)*2.0)/2
+					     -Math.pow(2*(colorSaturation-0.5),(5-difficulty)*2.0)/2;
+				}
+				if(generator.nextDouble()>p){
 					locSummary[i]++;
 					colorSummary[j]++;
 				}
