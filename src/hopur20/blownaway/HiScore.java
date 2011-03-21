be112c1d7d9377984a38c2e18e9fc64ba2f294ca
Karl Tryggvason <ktryggvason@gmail.com>
2011-03-21 14:57:10 +0000

    HiScore klasi

---
+++ src/hopur20/blownaway/HiScore.java
@@ -0,0 +1,73 @@
+package hopur20.blownaway;
+
+import android.content.Context;
+
+public class HiScore {
+	
+public String[] names; 
+public int[] scores;
+private final Context context;
+
+public HiScore(Context context)
+{
+	this.names = getnames();
+	this.scores = getScores();
+	this.context = context;
+}
+
+private int[] getScores() 
+{
+    
+	int[] hiscores= new int[10];
+	for (int i=0; i!=10;i++)
+	{
+		hiscores[i]=10-i;	
+	}
+	return hiscores;
+}
+
+private String[] getnames()
+{
+	String[] names = new String[10];
+	for (int i=1; i!=11;i++)
+		{
+		names[i-1]="AAA";	
+		}
+	return names;                            
+	                            
+}
+
+public int compareScore(int score)
+{
+	int place=scores.length;
+	int hiscore=scores[scores.length-1];
+
+	while (score>hiscore && place!=0)
+	{
+		hiscore=scores[place-1];
+		place=place-1;
+		
+	}
+	
+	return place;
+}
+
+public void addScore(int score, String name)
+{
+	int place = compareScore(score);
+	if (place>=scores.length){return;}
+	else
+	{
+		for (int i=scores.length-1; i!=place;i--)
+		{
+			scores[i]=scores[i-1];
+			names[i]=names[i-1];
+		}
+		names[place]=name;
+		scores[place]=score;
+	}
+	
+}
+
+	
+}
